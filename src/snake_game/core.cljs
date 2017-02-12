ac23c4aef9e68ad1e1d18fb25c704a2643897382
Mathieu Rheaume <mathieu@codingrhemes.com>
2017-02-11 22:25:26 -0500

    Improved the snake rendering

--- src/snake_game/core.cljs
+++ src/snake_game/core.cljs
@@ -13,7 +13,7 @@
 (rf/reg-event-db              ;; sets up initial application state
   :initialize
   (fn [_ _]
-    {:board-size [5,5]
+    {:board-size [5,10]
      :snake snake
      :game-running? true}))
 
@@ -22,21 +22,41 @@
   (fn [db _]
     (:board-size db)))
 
-(def cell-style {:style {:display "table-cell"
-                                 :height 50
-                                 :width 50
-                                 :color "white"
-                                 :border "2px solid black"}})
+(rf/reg-sub
+  :snake
+  (fn [db _]
+    (:snake db)))
 
-(defn single-row [pos_x size_y]
+(rf/reg-sub
+  :snake-pos
+  (fn [db _]
+    (:position (:snake db))))
+
+(def code->key
+  "map from a character code (read from events with event.which)"
+  {37 "left"
+   38 "up"
+   39 "right"
+   40 "down"})
+
+(def cell-style {:style {:display "table-cell" :position "relative"}})
+(def base-tile-style {:display "block" :height 75 :width 75 })
+(def background-tile-style (merge base-tile-style {:position "relative" :z-index -1}))
+(def worm-tile-style (merge base-tile-style {:position "absolute" :z-index 1 :top 0 :left 0}))
+
+(defn single-row [pos_x size_y snake-position]
   [:div {:key pos_x :style {:display "table-row"}}
    (for [pos_y (range size_y)]
-     [:div (merge {:key [pos_x, pos_y]} cell-style)])])
+     [:div (merge {:key [pos_x, pos_y]} cell-style)
+       [:img {:style background-tile-style :src "grass.png" }]
+       (if (= snake-position [pos_x, pos_y])
+         [:img {:style worm-tile-style :src "snake_start.png" }])
+      ])])
 
 (defn render-rows [board-size]
   [:div
    (for [pos_x (range (first board-size))]
-     (single-row pos_x (last board-size)))])
+     (single-row pos_x (last board-size) @(rf/subscribe [:snake-pos])))])
 
 (defn ui
   []
@@ -45,8 +65,14 @@
    (render-rows @(rf/subscribe [:board-size]))
    ])
 
+(defn keydown [e]
+  (if (contains? [37 38 39 40] (.-keyCode e))
+    (rf/dispatch [:keypressed e])
+    (.preventDefault e)))
+
 (defn ^:export run
   []
+  (set! (.-onkeydown js/document) keydown)
   (rf/dispatch-sync [:initialize])     ;; puts a value into application state
   (reagent/render [ui]              ;; mount the application's ui into '<div id="app" />'
                   (js/document.getElementById "app")))
